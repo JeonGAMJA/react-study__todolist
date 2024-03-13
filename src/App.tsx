ba8715c6a3498b2bade1a8f11244c0a1ba1da9fe
GamjaJEON <indian_potato@naver.com>
2024-03-13 16:04:08 +0900

    feat:........

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 
 import './App.css'
+import TodoForm from './components/TodoForm/TodoForm'
+import TodoList from './components/TodoList/TodoList'
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch()
+  const todos = useSelector((state: RootState) => state)
+
   return (
     <>
-      <div></div>
+      <TodoForm />
+      <TodoList></TodoList>
     </>
   )
 }
